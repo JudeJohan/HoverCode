ea28ece52fa11c3202cff7b58ea9780262b0659e
JudeJohan <jae91m@gmail.com>
2015-10-06 12:06:38 +0200

    Adding Nilssons Stuffs

---
+++ HoverX/app/src/main/java/com/rc/hover/hoverx/DriveActivity.java
@@ -0,0 +1,15 @@
+package com.rc.hover.hoverx;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Johannes on 2015-10-05.
+ */
+public class DriveActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drive);
+    }
+}
---
+++ HoverX/app/src/main/java/com/rc/hover/hoverx/MenuActivity.java
@@ -0,0 +1,67 @@
+package com.rc.hover.hoverx;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        Button connect = (Button) findViewById(R.id.connect_button);
+        final Button drive = (Button) findViewById(R.id.drive_button);
+        drive.setEnabled(false);
+        final TextView connect_status = (TextView) findViewById(R.id.connect_text);
+
+        connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               connect_status.setText("Connected!");
+                connect_status.setTextColor(getResources().getColor(R.color.LIME));
+                drive.setEnabled(true);
+            }
+        });
+
+
+
+    }
+
+    public void enable_drive(View v){
+        Intent drive_activity = new Intent(this, DriveActivity.class);
+        startActivity(drive_activity);
+    };
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
