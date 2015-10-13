ba43accfe5d89f3875da70f8a9db7102fa807288
JudeJohan <jae91m@gmail.com>
2015-10-13 11:47:55 +0200

    Revert "nfaefeanonoi"
    This reverts commit 1dbe6389e0b61a7fb36bce04b8dcc5f992834b3b.

--- HoverX/app/src/main/java/com/rc/hover/hoverx/DriveActivity.java
+++ HoverX/app/src/main/java/com/rc/hover/hoverx/DriveActivity.java
@@ -1,17 +1,23 @@
 package com.rc.hover.hoverx;
 
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ViewTreeObserver;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 /**
  * Created by Johannes on 2015-10-05.
  */
 public class DriveActivity extends AppCompatActivity {
+    VerticalSeekBar vsb1, vsb2 = null;
+    TextView tx1, tx2 = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drive);
-<<<<<<< HEAD
 
         vsb1 = (VerticalSeekBar)findViewById(R.id.seekBar);
         vsb2 = (VerticalSeekBar)findViewById(R.id.seekBar2);
@@ -54,7 +60,5 @@ public class DriveActivity extends AppCompatActivity {
             }
         });
 
-=======
->>>>>>> parent of 3c1cd6e... added vertical seekbars for drive view
     }
 }
