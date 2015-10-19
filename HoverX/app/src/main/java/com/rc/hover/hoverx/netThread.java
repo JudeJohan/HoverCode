7a883c9d9b4f89ed66c5d8fae818bef59f83a5ba
JudeJohan <jae91m@gmail.com>
2015-10-19 15:47:50 +0200

    server changes

--- HoverX/app/src/main/java/com/rc/hover/hoverx/netThread.java
+++ HoverX/app/src/main/java/com/rc/hover/hoverx/netThread.java
@@ -1,6 +1,10 @@
 package com.rc.hover.hoverx;
 
+import android.os.Handler;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
@@ -14,31 +18,65 @@ public class netThread implements Runnable {
     public Socket socket = null;
     public ServerSocket serverSocket = null;
 
+    Handler updateConversationHandler;
+
     public netThread(boolean isServer, int port, InetAddress serverAddr) {
         _isServer = isServer;
         _serverAddr = serverAddr;
         _port = port;
-        if(_isServer) {
+    }
+
+    @Override
+    public void run() {
+        if (_isServer) {
             try {
-                serverSocket = new ServerSocket(port);
-                socket = serverSocket.accept();
+                serverSocket = new ServerSocket(_port);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-        else {
+        } else {
             try {
                 socket.connect(new InetSocketAddress(_serverAddr, _port), 5000);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+        while (!Thread.currentThread().isInterrupted()) {
+            if(_isServer) {
+                try {
+                    socket = serverSocket.accept();
+                    commThread comThread = new commThread(socket);
+                    new Thread(comThread).start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
-    @Override
-    public void run() {
-        while(!Thread.currentThread().isInterrupted()) {
+    class commThread implements Runnable {
+        private Socket _clientSocket;
+        private BufferedReader _input;
 
+        public commThread(Socket clientSocket) {
+            _clientSocket = clientSocket;
+            try {
+                _input = new BufferedReader(new InputStreamReader(_clientSocket.getInputStream()));
+            } catch (IOException e) {
+
+            }
+        }
+
+        @Override
+        public void run() {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    String read = _input.readLine();
+                    //updateConversationHandler.post(new updateUIThread(read));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
