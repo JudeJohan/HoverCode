99b26535105625b203a7de6bd0b770fd80c6248e
tp4028 <tp4028>
2015-10-21 16:03:00 +0200

    fixed wifi

--- HoverX/app/src/main/java/com/rc/hover/hoverx/DriveActivity.java
+++ HoverX/app/src/main/java/com/rc/hover/hoverx/DriveActivity.java
@@ -3,6 +3,7 @@ package com.rc.hover.hoverx;
 
 import android.graphics.Canvas;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.ViewTreeObserver;
 import android.widget.SeekBar;
@@ -17,14 +18,17 @@ import com.rc.hover.hoverx.DataInfo.intDataInfo;
 public class DriveActivity extends AppCompatActivity {
     VerticalSeekBar vsb1, vsb2 = null;
     TextView tx1, tx2 = null;
-    HoverApp app = ((HoverApp)getApplication());
+    HoverApp app;
 
+    Handler m_handler;
+    Runnable m_handlerTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drive);
 
+        app = ((HoverApp)getApplication());
         vsb1 = (VerticalSeekBar)findViewById(R.id.seekBar);
         vsb2 = (VerticalSeekBar)findViewById(R.id.seekBar2);
         tx1 = (TextView)findViewById(R.id.textView);
@@ -36,7 +40,6 @@ public class DriveActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 tx1.setText("Current value: " + i);
-                app.dataHolder.add(new intDataInfo(DataInfo.ID.LeftDrive, DataInfo.TYPE.Integer, i));
             }
 
             @Override
@@ -54,7 +57,6 @@ public class DriveActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 tx2.setText("Current value: " + i);
-                app.dataHolder.add(new intDataInfo(DataInfo.ID.RightDrive, DataInfo.TYPE.Integer, i));
 
             }
 
@@ -65,8 +67,21 @@ public class DriveActivity extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                ((VerticalSeekBar)seekBar).setProgressAndThumb(50);
+                ((VerticalSeekBar) seekBar).setProgressAndThumb(50);
             }
         });
+
+        m_handler = new Handler();
+        m_handlerTask = new Runnable() {
+            @Override
+            public void run() {
+                m_handler.postDelayed(m_handlerTask, 100);
+                app.dataHolder.add(new intDataInfo(DataInfo.ID.LeftDrive, DataInfo.TYPE.Integer,
+                        vsb1.getProgress()));
+                app.dataHolder.add(new intDataInfo(DataInfo.ID.RightDrive, DataInfo.TYPE.Integer,
+                        vsb2.getProgress()));
+            }
+        };
+        m_handler.postDelayed(m_handlerTask, 100);
     }
 }
